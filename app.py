43a43ed34062f2fd01c384240d37763b83195748
admin <shraddhawork45@gmail.com>
2023-10-22 17:10:35 +0530

    New Commit

--- app.py
+++ app.py
@@ -10,21 +10,45 @@ df['month'] = df['date'].dt.month
 df['year'] = df['date'].dt.year
 
 def load_overall_analysis():
+    st.title('Overall Analysis')
 
+    # total invested amount
     total = round(df['amount'].sum())
-    max = df.groupby('startup')['amount'].max().sort_values(ascending=False).head(1).values[0]
-    mean = df.groupby('startup')['amount'].sum().mean()
-    num_startups= df['startup'].nunique()
+    # max amount infused in a startup
+    max_funding = df.groupby('startup')['amount'].max().sort_values(ascending=False).head(1).values[0]
+    # avg ticket size
+    avg_funding = df.groupby('startup')['amount'].sum().mean()
+    # total funded startups
+    num_startups = df['startup'].nunique()
 
     col1,col2,col3,col4 = st.columns(4)
+
     with col1:
-        st.metric('Total', str(total) + 'Cr')
+        st.metric('Total',str(total) + ' Cr')
     with col2:
-        st.metric('Max', str(max) + 'Cr')
+        st.metric('Max', str(max_funding) + ' Cr')
+
     with col3:
-        st.metric('Average Funding', str(round(mean)) + 'Cr')
+        st.metric('Avg',str(round(avg_funding)) + ' Cr')
+
     with col4:
-        st.metric('Count',num_startups)
+        st.metric('Funded Startups',num_startups)
+
+    st.header('MoM graph')
+    selected_option = st.selectbox('Select Type',['Total','Count'])
+    if selected_option == 'Total':
+        temp_df = df.groupby(['year', 'month'])['amount'].sum().reset_index()
+    else:
+        temp_df = df.groupby(['year', 'month'])['amount'].count().reset_index()
+
+    temp_df['x_axis'] = temp_df['month'].astype('str') + '-' + temp_df['year'].astype('str')
+
+    fig3, ax3 = plt.subplots()
+    ax3.plot(temp_df['x_axis'], temp_df['amount'])
+
+    st.pyplot(fig3)
+
+
 
 def load_investor_details(investor):
     st.title(investor)
@@ -68,10 +92,7 @@ st.sidebar.title('Startup Funding Analysis')
 option = st.sidebar.selectbox('Select One',['Overall Analysis','StartUp','Investor'])
 
 if option == 'Overall Analysis':
-    st.title('Overall Analysis')
-    btn0 = st.sidebar.button('Show Overall Analysis')
-    if btn0:
-        load_overall_analysis()
+    load_overall_analysis()
 
 elif option == 'StartUp':
     st.sidebar.selectbox('Select StartUp',sorted(df['startup'].unique().tolist()))
